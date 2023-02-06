87d76583bffe9f87b18a7f85f81a9d419c5b7ad3
Killigo <Killigo@mail.ru>
2023-02-06 20:33:24 +0300

    fix
    1. Alarm - Не оставляйте комментария в коде 2. Нет смысла создавать 2 корутины для изменения звука в них дубляж кода

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -1,9 +1,5 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Events;
 
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(Animator))]
@@ -12,14 +8,12 @@ public class Alarm : MonoBehaviour
 {
     [SerializeField] private Home _home;
 
-    private Coroutine _alarmStart;
-    private Coroutine _alarmStop;
+    private Coroutine _coroutine;
     private Animator _animator;
     private AudioSource _audio;
     private float _minVolume = 0f;
     private float _maxVolume = 1f;
     private float _changeRate = 0.5f;
-    private float _currentVolume;
     private int _alarmHash = Animator.StringToHash("Alarm");
 
 
@@ -27,7 +21,6 @@ public class Alarm : MonoBehaviour
     {
         _animator = GetComponent<Animator>();
         _audio = GetComponent<AudioSource>();
-        _currentVolume = _audio.volume;
     }
 
     private void OnEnable()
@@ -42,73 +35,42 @@ public class Alarm : MonoBehaviour
 
     public void Alarmed(bool alarm)
     {
-        if (alarm && _currentVolume < _maxVolume)
+        if (_coroutine != null)
         {
-            _alarmStart = StartCoroutine(AlarmUp());
+            StopCoroutine(_coroutine);
         }
 
-        if (!alarm && _currentVolume > _minVolume)
-        {
-            _alarmStop = StartCoroutine(AlarmDown());
-        }
+        _coroutine = StartCoroutine(AlarmState(alarm));
     }
 
-    private IEnumerator AlarmUp()
+    private IEnumerator AlarmState(bool alarm)
     {
-        if (_alarmStop != null)
+        if (alarm && _audio.volume < _maxVolume)
         {
-            StopCoroutine(_alarmStop);
+            if (!_audio.isPlaying)
+            {
+                _audio.Play();
+                _animator.SetBool(_alarmHash, true);
+            }
+
+            while (_audio.volume < _maxVolume)
+            {
+                _audio.volume = Mathf.MoveTowards(_audio.volume, _maxVolume, _changeRate * Time.deltaTime);
+
+                yield return null;
+            }
         }
-
-        if (!_audio.isPlaying)
+        else if (!alarm && _audio.volume > _minVolume)
         {
-            _audio.Play();
-            _animator.SetBool(_alarmHash, true);
-        }
+            while (_audio.volume > _minVolume)
+            {
+                _audio.volume = Mathf.MoveTowards(_audio.volume, _minVolume, _changeRate * Time.deltaTime);
 
-        while (_currentVolume < _maxVolume)
-        {
-            _currentVolume = Mathf.MoveTowards(_currentVolume, _maxVolume, _changeRate * Time.deltaTime);
-            _audio.volume = _currentVolume;
+                yield return null;
+            }
 
-            yield return null;
+            _animator.SetBool(_alarmHash, false);
+            _audio.Stop();
         }
     }
-
-    private IEnumerator AlarmDown()
-    {
-        StopCoroutine(_alarmStart);
-
-        while (_currentVolume > _minVolume)
-        {
-            _currentVolume = Mathf.MoveTowards(_currentVolume, _minVolume, _changeRate * Time.deltaTime);
-            _audio.volume = _currentVolume;
-
-            yield return null;
-        }
-
-        _animator.SetBool(_alarmHash, false);
-        _audio.Stop();
-    }
-
-    //private void Update()
-    //{
-    //    if (_isAlarm && _currentVolume < _maxVolume)
-    //    {
-    //        _currentVolume = Mathf.MoveTowards(_currentVolume, _maxVolume, _changeRate * Time.deltaTime);
-    //        _audio.volume = _currentVolume;
-    //    }
-
-    //    if (!_isAlarm && _currentVolume > _minVolume)
-    //    {
-    //        _currentVolume = Mathf.MoveTowards(_currentVolume, _minVolume, _changeRate * Time.deltaTime);
-    //        _audio.volume = _currentVolume;
-    //    }
-
-    //    if (_audio.volume == _minVolume)
-    //    {
-    //        _animator.SetBool(_alarmHash, false);
-    //        _audio.Stop();
-    //    }
-    //}
 }
